e95a1938a8813c8f21f1c1cf69cbc45fa4819442
藤島太智 <fujishimataichi@fujishimataichinoMacBook-puro.local>
2021-01-31 16:41:26 +0900

    edit

--- public_html/kadai_2-5.php
+++ public_html/kadai_2-5.php
@@ -5,91 +5,126 @@
 <title>sample</title>
 </head>
 <body>
-    <form action="kadai_2-5.php" method="post">
-        名前：<br />
-        <input type="text" name="name" size="30" value="" placeholder="名前" /><br />
-        <br />
-        コメント：<br />
-        <textarea type="text" name="comment" size="30" value="" placeholder="コメント" ></textarea><br />
-        <br />
-        削除：<br />
-        <input type="number" name="edit" size="30" value="" placeholder="編集する番号" /><br />
-        <br />
-        <input type="submit" value="登録する" />
-        <input type="submit" value="編集する" />
-    </form>
     <?php
 
         $filename = "kadai_2-5.txt";
 
-        if((isset($_POST["edit"])) && ($_POST["edit"] != "")) {
-
-            $edit = $_POST['edit'];
-
-            $fp = fopen($filename, "r");
-            while (!feof($fp)){
-                $e_lines[] = fgets($fp);
-            }
-            fclose($fp);
-
-            $fp = fopen($filename, "w");
-            foreach($e_lines as $e_line) {
-                $edi = explode("<>",$e_line);
-                if($edi[0] == $edit)
-                {
-                    $newname=$edi[1];
-                    $newcomment=$edi[2];
-                    var_dump($edi);
-                    fwrite($fp, $edit."<>".$newname."<>".trim($newcomment)."<>".date('Y-m-d'));
-                }else{
-                    fwrite($fp, $e_line);
-                }
-            }
-            fclose($fp);
-
-        }
-        
+        // 新規投稿 
         if((isset($_POST['name'])) && (isset($_POST['comment']))) {
-
-            if(((($_POST['name']) != "") || (($_POST['comment']) != "")) && !isset($_POST['edit'])){
+            
+            if(((($_POST['name']) != "") || (($_POST['comment']) != ""))){
                 $name=$_POST['name'];
                 $comment=$_POST['comment'];
-                // ファイルを全て配列に入れる
-                if(file_exists($filename)){
-                    $text = file_get_contents($filename);
-                }else{
-                    $text = "";
-                }
-                
-                $text_rows = explode("\n",$text);//改行で分解
-                $count = count($text_rows);//行数をカウント
-                $add_text = $count."<>".$name."<>".trim($comment)."<>".date('Y-m-d');//追加したい文章
-                // array_unshift($text_rows, $add_text);//配列の先頭に追加
-                array_push($text_rows, $add_text);
-                $write_text = implode("\n", $text_rows);//改行でくっつける
-                file_put_contents($filename, $write_text);
-                
-                foreach ($text_rows as $text_row) {
-                    if($text_row === ""){
-                        continue;
-                    }
-                    $ret = explode("<>", $text_row);
-                    echo $ret[0];
-                    echo $ret[1];
-                    echo $ret[2];
-                    echo $ret[3];
-                    echo "<br>";
-                }
-        
-            }else{
 
+                if (empty($_POST['edit-number'])) {
+                // ファイルを全て配列に入れる
+                    if(file_exists($filename)){
+                        $text = file_get_contents($filename);
+                    }else{
+                        $text = "";
+                    }
+                        
+                    $text_rows = explode("\n",$text);//改行で分解
+                    $count = count($text_rows);//行数をカウント
+                    $add_text = $count."<>".$name."<>".trim($comment)."<>".date('Y-m-d');//追加したい文章
+                    // array_unshift($text_rows, $add_text);//配列の先頭に追加
+                    array_push($text_rows, $add_text);
+                    $write_text = implode("\n", $text_rows);//改行でくっつける
+                    file_put_contents($filename, $write_text);
+            
+                }else{
+
+                    // 編集
+                    $edit_number = $_POST['edit-number'];
+
+                    $e_lines=file($filename);
+
+                    $fp = fopen($filename, "w");
+                    foreach($e_lines as $e_line) {
+                        $edi = explode("<>",$e_line);
+                        if($edi[0] == $edit_number){
+                            fwrite($fp, $edit_number."<>".$name."<>".trim($comment)."<>".date('Y-m-d')."\n");
+                        }else{
+                            fwrite($fp, $e_line);
+                        }
+                    }
+                    fclose($fp);
+                    $edit_flag=0;
+                
+                }
+
+            }else{
+                            
                 echo "<br>"."名前またはコメントが入力されていません。";
                 
             }
-        
-
         }
 
+        // 編集選択
+        if (!empty($_POST['editnum'])) {
+
+            //入力データの受け取りを変数に代入
+                $edit = $_POST['editnum'];
+         
+            //読み込んだファイルの中身を配列に格納する
+                $editCon = file($filename);
+         
+                foreach ($editCon as $ed_line) {  
+                    $editdata = explode("<>",$ed_line);
+                
+                    //投稿番号と編集対象番号が一致したらその投稿の「名前」と「コメント」を取得
+                    if ($edit == $editdata[0]) {
+            
+                    //投稿のそれぞれの値を取得し変数に代入
+                        $editnumber = $editdata[0];
+                        $editname = $editdata[1];
+                        $editcomment = $editdata[2];
+            
+                    //既存の投稿フォームに、上記で取得した「名前」と「コメント」の内容が既に入っている状態で表示させる
+                    //formのvalue属性で対応
+                    }
+                }
+         }
+            
+    ?>
+    <form action="kadai_2-5.php" method="post">
+        名前：<br />
+        <input type="text" name="name" size="30" placeholder="名前" value="<?php if(isset($editname)){ 
+                                                                                        echo $editname;
+                                                                                    } ?>" ><br />
+        <br />
+        コメント：<br />
+        <input type="text" name="comment" size="30" placeholder="コメント" value="<?php if(isset($editcomment)){
+                                                                                                    echo $editcomment;
+                                                                                            } ?>" >
+        <br />
+        <input type="hidden" name="edit-number" value="<?php if(isset($editnumber)) {echo $editnumber;} ?>">
+        <input type="submit" value="登録する" />
+        <br>
+        
+    </form>
+    <form action="kadai_2-5.php" method="post">
+        編集：<br />
+        <input type="text" name="editnum" placeholder="編集する番号">
+        <input type="submit" value="編集する" />
+    </form>
+
+    <?php
+        if(file_exists($filename)){
+            $array=file($filename);
+
+            foreach ($array as $arr) {
+                if($arr === ""){
+                    continue;
+                }
+                $ret = explode("<>", $arr);
+                echo $ret[0];
+                echo $ret[1];
+                echo $ret[2];
+                echo $ret[3];
+                echo "<br>";
+            }
+        }
     ?>
 </body>
 </html>
---
+++ public_html/kadai_2-6.php
@@ -0,0 +1,172 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+<meta charset="UTF-8">
+</head>
+<body>
+
+
+    <?php
+        $filename = "kadai_2-6.txt";
+
+
+                    //新規投稿機能  
+        if (!empty($_POST['name']) && !empty($_POST['comment']) && !empty($_POST['passcode'])) {
+
+            $name = $_POST['name'];
+            $comment = $_POST['comment'];
+            $date = date("Y年m月d日 H時i分s秒");
+            $password=$_POST['passcode'];
+
+
+            //編集対象番号が入力されているなら
+            if (empty($_POST['edit-number'])) {
+            //ファイルの存在がある場合は投稿番号+1、なかったら1を指定する
+            if (file_exists($filename)) {
+            $number = count(file($filename)) + 1;
+            } else {
+            $number = 1;
+            }
+
+
+
+        $list = $number . "<>" . $name . "<>" . $comment . "<>" . $date."<>".$password."<>";  //投稿内容
+
+
+            //ファイルを追記保存モードで開く
+            $fp = fopen($filename,"a");
+            fwrite($fp,$list . "\n");
+            fclose($fp);
+
+
+
+
+                //編集実行機能  つまり新規投稿と編集で分岐する
+        }else{
+
+            $edit_number = $_POST['edit-number']; //読み込んだファイルの中身を配列に格納する
+            $ret_array = file($filename);
+            $fp = fopen($filename,"w");     //ファイルを開き、中身を空に 
+
+            foreach ($ret_array as $line) {   //配列の数だけループ
+            $edit_date= explode("<>",$line);
+
+                //新稿番号と編集番号が一致・不一致で分ける
+            if ($edit_date[0] == $edit_number) {
+
+                //編集のフォームで送信された値で上書きする（$edit_numberが$numberと変わっている）
+                fwrite($fp, $edit_number. "<>" . $name . "<>" . $comment . "<>" . $date . "\n");
+
+            } else {
+                fwrite($fp,$line);  //不一致なら書き込む
+            }
+            }
+                fclose($fp);
+
+            }
+        }  
+
+
+
+                            //削除機能
+        if(!empty($_POST['deleteNo']) && !empty($_POST['Delpasscode'])){  //DelpasswordがPOSTされていないなら、機能しない
+
+
+            $Delpassword=$_POST['Delpasscode'] ;
+            $delete=$_POST['deleteNo']; //$deleteの定義づけ
+            $delcon=file("kadai_2-6.txt"); //file関数で開くテキストファイルの指定
+            $fp=fopen("kadai_2-6.txt","w");//ファイル読み込み、中身を空にする
+
+
+            for($j=0; $j<count($delcon); $j++){ //ループ処理を行う
+            $deldata=explode("<>",$delcon[$j]); //カッコで抽出
+
+
+            if($deldata[0] != $delete){ //削除番号と行番号が一致・不一致
+            fwrite($fp,$delcon[$j]); //行内容をファイルに書き込む
+
+            }else{
+            fwrite($fp, ""); //書き込まない（つまり削除）、行を詰める
+            }
+            }
+
+            fclose($fp); //ファイルを閉じる
+        }
+
+
+
+
+                //編集選択機能
+        if (!empty($_POST['edit']) &&!empty($_POST['Editpasscode'])) {
+
+            //入力データの受け取りを変数に代入
+            $Editpassword= $_POST['Editpasscode'];
+            $edit = $_POST['edit'];
+
+            //読み込んだファイルの中身を配列に格納する
+            $editCon = file($filename);
+
+            foreach ($editCon as $line) {  
+                $editdata = explode("<>",$line);
+
+                //投稿番号と編集対象番号が一致したらその投稿の「名前」と「コメント」を取得
+                    if ($edit == $editdata[0]) {
+
+                //投稿のそれぞれの値を取得し変数に代入
+                    $editnumber = $editdata[0];
+                    $editname = $editdata[1];
+                    $editcomment = $editdata[2];
+
+                //既存の投稿フォームに、上記で取得した「名前」と「コメント」の内容が既に入っている状態で表示させる
+                //formのvalue属性で対応
+                }
+            }
+        }
+    ?>
+
+
+
+
+    <form action="kadai_2-6.php" method="post">
+    <input type="text" name="name" placeholder="名前" value="<?php if(isset($editname)) {echo $editname;} ?>"><br>
+    <input type="text" name="comment" placeholder="コメント" value="<?php if(isset($editcomment)) {echo $editcomment;} ?>">
+    <input type="hidden" name="edit-number" value="<?php if(isset($editnumber)) {echo $editnumber;} ?>">
+    <input id="password" type="password" name="passcode" value="<?php if(isset($editpassword)) {echo $editpassword;} ?>" placeholder="パスワードを入力してください">
+    <input type="submit" name="submit" value="送信">
+    </form>
+
+
+    <form action="kadai_2-6.php" method="post">
+    <input type="text" name="deleteNo" placeholder="削除対象番号">
+    <input id="password" type="password" name="Delpasscode"  value="" placeholder="パスワードを入力してください">
+    <input type="submit" name="delete" value="削除">
+    </form>
+
+    <form action="kadai_2-6.php" method="post">
+    <input type="text" name="edit" placeholder="編集対象番号">
+    <input id="password" type="password" name="Editpasscode"  value="" placeholder="パスワードを入力してください">
+    <input type="submit" value="編集">
+    </form>
+
+
+
+    <?php      
+
+
+                        //表示機能
+        $filename = "kadai_2-6.txt";
+        if (file_exists($filename)) { 
+            $array = file($filename); //読み込んだファイルの中身を配列に格納する
+
+
+            foreach ($array as $word) { //取得したファイルデータを全てループ処理で表示する
+            $getdata = explode("<>",$word);  //explode関数でそれぞれの値を取得
+            echo $getdata[0] . " " . $getdata[1] . " " . $getdata[2] . " " . $getdata[3] . $getdata[4] . " " ."<br>"; //取得した値を表示する
+            }
+        }
+
+
+    ?>
+
+  </body>
+</html>
